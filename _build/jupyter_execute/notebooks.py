7421db196104523c13e776f2ad2b0a558d6b1338
rkostov <robertokostov3@gmail.com>
2021-06-08 19:11:47 +0200

    Add modified version of the book v1

--- _build/jupyter_execute/notebooks.py
+++ _build/jupyter_execute/notebooks.py
@@ -1,76 +1,355 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Content with notebooks
-# 
-# You can also create content with Jupyter Notebooks. This means that you can include
-# code blocks and their outputs in your book.
-# 
-# ## Markdown + notebooks
-# 
-# As it is markdown, you can embed images, HTML, etc into your posts!
-# 
-# ![](https://myst-parser.readthedocs.io/en/latest/_static/logo.png)
-# 
-# You can also $add_{math}$ and
-# 
-# $$
-# math^{blocks}
-# $$
-# 
-# or
-# 
-# $$
-# \begin{aligned}
-# \mbox{mean} la_{tex} \\ \\
-# math blocks
-# \end{aligned}
-# $$
-# 
-# But make sure you \$Escape \$your \$dollar signs \$you want to keep!
-# 
-# ## MyST markdown
-# 
-# MyST markdown works in Jupyter Notebooks as well. For more information about MyST markdown, check
-# out [the MyST guide in Jupyter Book](https://jupyterbook.org/content/myst.html),
-# or see [the MyST markdown documentation](https://myst-parser.readthedocs.io/en/latest/).
-# 
-# ## Code blocks and outputs
-# 
-# Jupyter Book will also embed your code blocks and output in your book.
-# For example, here's some sample Matplotlib code:
+# ### [Линк](https://colab.research.google.com/drive/1mguVQuMEn2mIfISPCf4I9P6rvjNAK2ub?usp=sharing) до самиот Notebook
+
+# ### Вовед
+
+# #### Import на библиотеките кои се користат во кодот
 
 # In[1]:
 
 
-from matplotlib import rcParams, cycler
+from keras.models import Model
+from keras.layers import Dense, Input, Dropout, LSTM, Activation
+from keras.layers.embeddings import Embedding
+from sklearn.metrics import classification_report, f1_score, log_loss, precision_score, recall_score
+
+import csv
 import matplotlib.pyplot as plt
 import numpy as np
-plt.ion()
+import pandas as pd
+
+np.random.seed(1)
+
+
+# #### Симнување на glove векторите за репрезентација на зборови
+
+# In[2]:
+
+
+get_ipython().system('wget http://nlp.stanford.edu/data/glove.6B.zip')
+get_ipython().system('unzip glove*.zip')
+
+
+# ### Помошни функции
+
+# #### Исчитување на glove фајлот со вредности
+
+# In[ ]:
+
+
+def read_glove_vecs(glove_file):
+    with open(glove_file, 'r', encoding="utf8") as f:
+        words = set()
+        word_to_vec_map = {}
+        for line in f:
+            line = line.strip().split()
+            curr_word = line[0]
+            words.add(curr_word)
+            word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+
+        i = 1
+        words_to_index = {}
+        index_to_words = {}
+        for w in sorted(words):
+            words_to_index[w] = i
+            index_to_words[i] = w
+            i = i + 1
+            
+    return words_to_index, index_to_words, word_to_vec_map
+
+
+# ##### Softmax функцијата
+
+# In[ ]:
+
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    
+    return e_x / e_x.sum()
+
+
+# ##### Читање на соодветните вредности од CSV train и test датотеките
+
+# In[ ]:
+
+
+def read_csv(filename):
+    phrase = []
+    emoji_ = []
+
+    with open(filename) as csvDataFile:
+        csv_reader = csv.reader(csvDataFile)
+
+        for row in csv_reader:
+            phrase.append(row[0])
+            emoji_.append(row[1])
+
+    x = np.asarray(phrase)
+    y = np.asarray(emoji_, dtype=int)
+
+    return x, y
+
+
+# ##### Излезите(бројки) ги претвора во one-hot вектори
+
+# In[ ]:
+
+
+def convert_to_one_hot(y, c):
+    y = np.eye(c)[y.reshape(-1)]
+    
+    return y
+
+
+# ##### Предвидување на излезите при дадени елементи како влез
+
+# In[ ]:
+
+
+def predict(X, Y, W, b, word_to_vec_map):
+    m = X.shape[0]
+    pred = np.zeros((m, 1))
+
+    for j in range(m):
+        words = X[j].lower().split()
+
+        avg = np.zeros((50,))
+        for w in words:
+            avg += word_to_vec_map[w]
+        avg = avg / len(words)
+
+        z = np.dot(W, avg) + b
+        a = softmax(z)
+        pred[j] = np.argmax(a)
+
+    print("Accuracy: " + str(np.mean((pred[:] == Y.reshape(Y.shape[0], 1)[:]))))
+
+    return pred
+
+
+# ##### Претворање на реченици дадени како влез во матрици од вредности
+
+# In[ ]:
+
+
+def sentences_to_indices(X, word_to_index, max_len):
+    m = X.shape[0] 
+    x_indices = np.zeros((m, max_len))
+
+    for i in range(m):
+        sentence_words = (X[i].lower()).split()
+        j = 0
+    
+        for w in sentence_words:
+            # i-тата вредност е редниот број на реченицата, j-тата е редниот
+            # број на зборот во неа. вредноста која се поставува е таа на 
+            # соодветниот збор
+            x_indices[i, j] = word_to_index[w]
+            j = j + 1
+    
+    return x_indices
+
+
+# ##### Креирање на веќе истрениран Embedding слој со помош на glove векторите
+
+# In[ ]:
+
+
+def pretrained_embedding_layer(word_to_vec_map, word_to_index):
+    vocab_len = len(word_to_index) + 1
+    emb_dim = word_to_vec_map["cucumber"].shape[0]
+    emb_matrix = np.zeros((vocab_len, emb_dim))
+
+    for word, index in word_to_index.items():
+        emb_matrix[index, :] = word_to_vec_map[word]
+
+    embedding_layer = Embedding(vocab_len, emb_dim)
+    embedding_layer.build((None,))
+    embedding_layer.set_weights([emb_matrix])
+
+    return embedding_layer
+
+
+# ##### Креирање на модел со соодветни предефинирани вредности
+
+# In[ ]:
+
+
+def sentiment_analysis(input_shape, word_to_vec_map, word_to_index):
+    sentence_indices = Input(shape=input_shape, dtype=np.int32)
+    embedding_layer = pretrained_embedding_layer(word_to_vec_map, word_to_index)
+    embeddings = embedding_layer(sentence_indices)
+
+    # LSTM слој со 128-димензионален hidden state
+    X = LSTM(128, return_sequences=True)(embeddings)
+    
+    # Веројатност на зачувување од 0.5
+    X = Dropout(0.5)(X)
+    
+    # Уште еден LSTM слој со 128-димензионален hidden state
+    X = LSTM(128)(X)
+
+    X = Dropout(0.5)(X)
+    X = Dense(5, activation='softmax')(X)
+    X = Activation('softmax')(X)
+
+    model = Model(sentence_indices, X)
+
+    return model
+
+
+# # Main дел
+
+# ##### Читање на train и test податоците, пренос на излезите како one-hot вектори, читање на векторите за репрезентација на зборови
+
+# In[ ]:
+
+
+X_train, Y_train = read_csv('train_set.csv')
+X_test, Y_test = read_csv('test_set.csv')
+
+maxLen = len(max(X_train, key=len).split())
+
+Y_oh_train = convert_to_one_hot(Y_train, 5)
+Y_oh_test = convert_to_one_hot(Y_test, 5)
+
+word_to_index_, index_to_word, word_to_vec_map_ = read_glove_vecs('glove.6B.50d.txt')
+
+
+# ##### Креирање и тренирање на моделот
+
+# In[ ]:
+
+
+model = sentiment_analysis((maxLen,), word_to_vec_map_, word_to_index_)
+model.summary()
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+X_train_indices = sentences_to_indices(X_train, word_to_index_, maxLen)
+Y_train_oh = convert_to_one_hot(Y_train, 5)
+
+model.fit(X_train_indices, Y_train_oh, epochs=100, batch_size=32, shuffle=True)
+
+
+# ##### Тестирање на моделот со test податоците
+
+# In[ ]:
+
+
+X_test_indices = sentences_to_indices(X_test, word_to_index_, max_len=maxLen)
+Y_test_oh = convert_to_one_hot(Y_test, 5)
+
+loss, acc = model.evaluate(X_test_indices, Y_test_oh)
+print()
+print("Accuracy ", acc)
+
+
+# ##### Преглед на влезовите кои се грешно предвидени и дополнителни метрики за евалуација
+
+# In[ ]:
+
+
+y_test_oh = np.eye(5)[Y_test.reshape(-1)]
+X_test_indices = sentences_to_indices(X_test, word_to_index_, maxLen)
+pred = model.predict(X_test_indices)
+
+actual = []
+predicted = []
+
+for i in range(len(X_test)):
+    x = X_test_indices
+    num = np.argmax(pred[i])
+
+    actual.append(Y_test[i])
+    predicted.append(num)
+
+    if num != Y_test[i]:
+        print('Input: ' + str(X_test[i]))
+        print('Expected class: ' + str(Y_test[i]))
+        print('Predicted class: ' + str(num) + '\n')
+
+precision = precision_score(actual, predicted, average='macro')
+recall = recall_score(actual, predicted, average='macro')
+f1_score = f1_score(actual, predicted, average='macro')
+loss = log_loss(actual, pred, eps=1e-15)
+matrix = classification_report(actual, predicted, labels=[0, 1, 2, 3, 4])
+
+
+# ##### Тестирање на моделот со влезови од корисник
+
+# In[ ]:
+
+
+x_test = np.array(['very happy'])
+X_test_indices = sentences_to_indices(x_test, word_to_index_, maxLen)
+print('Input: ' + x_test[0])
+print('Predicted class: ' + str(np.argmax(model.predict(X_test_indices))) + '\n')
 
 
 # In[ ]:
 
 
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-N = 10
-data = [np.logspace(0, 1, 100) + np.random.randn(100) + ii for ii in range(N)]
-data = np.array(data).T
-cmap = plt.cm.coolwarm
-rcParams['axes.prop_cycle'] = cycler(color=cmap(np.linspace(0, 1, N)))
+x_test = np.array(['very sad'])
+X_test_indices = sentences_to_indices(x_test, word_to_index_, maxLen)
+print('Input: ' + x_test[0])
+print('Predicted class: ' + str(np.argmax(model.predict(X_test_indices))) + '\n')
 
 
-from matplotlib.lines import Line2D
-custom_lines = [Line2D([0], [0], color=cmap(0.), lw=4),
-                Line2D([0], [0], color=cmap(.5), lw=4),
-                Line2D([0], [0], color=cmap(1.), lw=4)]
-
-fig, ax = plt.subplots(figsize=(10, 5))
-lines = ax.plot(data)
-ax.legend(custom_lines, ['Cold', 'Medium', 'Hot']);
+# In[ ]:
 
 
-# There is a lot more that you can do with outputs (such as including interactive outputs)
-# with your book. For more information about this, see [the Jupyter Book documentation](https://jupyterbook.org)
+x_test = np.array(['i am starving'])
+X_test_indices = sentences_to_indices(x_test, word_to_index_, maxLen)
+print('Input: ' + x_test[0])
+print('Predicted class: ' + str(np.argmax(model.predict(X_test_indices))) + '\n')
+
+
+# In[ ]:
+
+
+x_test = np.array(['I have met the love of my life'])
+X_test_indices = sentences_to_indices(x_test, word_to_index_, maxLen)
+print('Input: ' + x_test[0])
+print('Predicted class: ' + str(np.argmax(model.predict(X_test_indices))) + '\n')
+
+
+# ##### Приказ на мерките за успешност на моделот
+
+# In[ ]:
+
+
+print('Accuracy: {0}'.format(str(acc)))
+
+
+# In[ ]:
+
+
+print('Log loss: {0}'.format(loss))
+
+
+# In[ ]:
+
+
+print('Precision: {0}'.format(precision))
+
+
+# In[ ]:
+
+
+print('Recall: {0}'.format(recall))
+
+
+# In[ ]:
+
+
+print('F1 score: {0}'.format(f1_score))
+
+
+# In[ ]:
+
+
+print('Classification report: \n{0}'.format(matrix))
+
